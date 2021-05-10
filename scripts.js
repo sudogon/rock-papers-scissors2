2d770f8136f2e0b0efde651434577e6981d409e2
atemoia <renannowak@hotmail.com>
2021-05-10 11:52:13 -0300

    Esqueleto do js ainda com bugs

--- scripts.js
+++ scripts.js
@@ -1,63 +1,99 @@
-
 const selections = ["pedra", "papel", "tesoura"];
 
-var computerSelection = computerPlay();
-var humanScore = 0;
+var playerScore = 0;
 var computerScore = 0;
 var isWinner = Boolean;
+
+const btns = document.querySelectorAll(".btn");
+btns.forEach((button) => button.addEventListener("click", playRound));
+
+// converte  pressionar o botao em uma string
+function convertBtn() {
+    let buttonId = e.target.id;
+    console.log(buttonId);
+    return buttonId.split("-")[0];
+}
 
 function computerPlay() {
     let selections = ["pedra", "papel", "tesoura"];
     return selections[Math.floor(Math.random() * selections.length)]; /*  */
 }
 
-function playRound() {
-    var playerSelection = prompt("Pedra! Papel! Tesoura! Vence quem ganhar 5!");
-    if (playerSelection === null) {
-        console.log(`Tchau`);
-    } else {
-        let humanPlay = playerSelection.toLowerCase();
-    computerSelection = computerPlay();
-    result = '';
+function playRound(e) {
+    var playerSelection = convertBtn();
+    var computerSelection = computerPlay();
 
-    if (playerSelection == computerSelection) {
+    updateChoices(playerSelection, computerSelection);
+
+    if (playerSelection === computerSelection) {
         result = 'É um empate!'
         return isWinner = false;
-    } else if ((humanPlay == "pedra" && computerSelection == "tesoura") || 
-             (humanPlay == "tesoura" && computerSelection == "papel") || 
-             (humanPlay == "papel" && computerSelection == "pedra")) {
-        humanScore++;
-        result = `${humanScore} - ${computerScore} Você ganhou a rodada! ${playerSelection} vence ${computerSelection}.`;
+    } else if ((playerSelection === "pedra" && computerSelection === "tesoura") || 
+            (playerSelection === "tesoura" && computerSelection === "papel") || 
+            (playerSelection === "papel" && computerSelection === "pedra")) {
+        playerScore++;
+        result = `Você ganhou a rodada! ${playerSelection} vence ${computerSelection}.`;
         return isWinner = true;
         
-    } else if ((humanPlay == "tesoura" && computerSelection == "pedra") || 
-             (humanPlay == "papel" && computerSelection == "tesoura") || 
-             (humanPlay == "pedra" && computerSelection == "papel")) {
+    } else if ((playerSelection === "tesoura" && computerSelection === "pedra") || 
+            (playerSelection === "papel" && computerSelection === "tesoura") || 
+            (playerSelection === "pedra" && computerSelection === "papel")) {
         computerScore++;
-        result = `${humanScore} - ${computerScore} Você perdeu a rodada. ${playerSelection} perde para ${computerSelection}.`;
+        result = `Você perdeu a rodada. ${playerSelection} perde para ${computerSelection}.`;
         return isWinner = false;
-    } 
     }
-    
 }
 
-function game () {
-    for (let i = 0; i < 20; i++) {        
-        playRound(computerSelection);
+function updateChoices(playerSelection, computerSelection) {
+    const playerIcon = document.getElementById("player-icon");
+    const computerIcon = document.getElementById("computer-icon");
 
-        if (isWinner === true) {
-            console.log(result);
-            if (humanScore == 5) {
-                console.log(`VOCÊ VENCEU!!! ${humanScore} - ${computerScore}`);
-                break;
-            }
-        } else {
-            console.log(result);
-            if (computerScore == 5) {
-                console.log(`Você perdeu. ${humanScore} - ${computerScore}`);
-                break;
-            }
+    playerIcon.classlist.add("active");
+    if (playerSelection === "pedra") {
+        var playerIconName = `fa-hand-rock`;
+    } else if (playerSelection === "papel") {
+        var playerIconName = `fa-hand-paper`;
+    } else {
+        var playerIconName = `fa-hand-scissors`;
+    }
+    playerIcon.classList = `far ${playerIconName}`;
+
+    computerIcon.classlist.add("active");
+    if (computerSelection === "pedra") {
+        var computerIconName = `fa-hand-rock`;
+    } else if (computerSelection === "papel") {
+        var computerIconName = `fa-hand-paper`;
+    } else {
+        var computerIconName = `fa-hand-scissors`;
+    }
+    computerIcon.classlist = `far ${computerIconName}`;
+}
+
+function updateResult (result) {
+    const roundResult = document.getElementById("result");
+    roundResult.textContent = result;
+}
+
+function updateScore (isWinner) {
+    if (isWinner === true) {
+        updateResult(result);
+        
+        const totalScore = document.getElementById("scores");
+        const endGame = document.getElementById("end-game");
+
+        totalScore.textContent = `${playerScore} - ${computerScore}`;
+        if (playerScore === 5) {
+            endGame.textContent = (`Você venceu! Pressione F5 para jogar de novo.`);
+        }
+    } else {
+        updateResult(result);
+        totalScore.textContent = `${playerScore} - ${computerScore}`;
+        if (computerScore === 5) {
+            endGame.textContent = (`Você perdeu. Pressione F5 para jogar de novo.`);
         }
     }
 }
-game();
+
+playRound(e);
+updateResult(result);
+updateScore(isWinner);
